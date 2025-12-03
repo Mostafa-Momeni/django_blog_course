4351fffa4f5a049a32b6fe9b0c5c346851137c98
Momeni <MMomeni@BPC.CO.IR>
2025-12-03 09:35:36 +0330

    complet project

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('email','age',)
+        fields = UserCreationForm.Meta.fields + ('email','age','photo',)
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm):
